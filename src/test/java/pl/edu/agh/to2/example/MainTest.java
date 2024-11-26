24969ec35d398a415992e744e1a75a766d3d8537
Tomasz <tomekkurcon1@gmail.com>
2024-11-26 19:03:07 +0100

    initial commit

---
+++ src/test/java/pl/edu/agh/to2/example/MainTest.java
@@ -0,0 +1,14 @@
+package pl.edu.agh.to2.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class MainTest {
+
+    @Test
+    public void testMain() {
+        assertTrue(true);
+    }
+}
