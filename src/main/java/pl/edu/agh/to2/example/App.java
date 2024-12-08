9cd15eb4cc0b1a2e4863e434d734e2dfe1dfa560
Tomasz <tomekkurcon1@gmail.com>
2024-12-08 21:04:22 +0100

    Changed way of starting app

--- src/main/java/pl/edu/agh/to2/example/App.java
+++ src/main/java/pl/edu/agh/to2/example/App.java
@@ -2,14 +2,14 @@ package pl.edu.agh.to2.example;
 
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import pl.edu.agh.to2.example.config.AppConfig;
-import pl.edu.agh.to2.example.view.SpringFXMLLoader;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import pl.edu.agh.to2.example.applicationEvent.StageReadyEvent;
 
 public class App extends Application {
-    private AnnotationConfigApplicationContext context;
+
+    private ConfigurableApplicationContext applicationContext;
 
 
     public static void main(String[] args) {
@@ -18,23 +18,17 @@ public class App extends Application {
 
     @Override
     public void init() {
-        context = new AnnotationConfigApplicationContext(AppConfig.class);
+        applicationContext = new SpringApplicationBuilder(SpringApp.class).run();
     }
-
 
     @Override
     public void start(Stage primaryStage) {
-        SpringFXMLLoader loader = context.getBean(SpringFXMLLoader.class);
-        Scene scene = loader.load("/fxml/MainView.fxml").scene();
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("File Explorer");
-        primaryStage.show();
+        applicationContext.publishEvent(new StageReadyEvent(primaryStage));
     }
 
     @Override
     public void stop() {
-        context.close();
+        applicationContext.stop();
         Platform.exit();
     }
-
 }
