f09fcb6a26c117460633dcf2f4062a0e1afd89f8
Filip Bieńkowski <fbienkowski@student.agh.edu.pl>
2024-12-15 15:11:03 +0100

    added import

--- src/main/java/pl/edu/agh/to2/gui/controller/FileListViewController.java
+++ src/main/java/pl/edu/agh/to2/gui/controller/FileListViewController.java
@@ -3,6 +3,7 @@ package pl.edu.agh.to2.gui.controller;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
